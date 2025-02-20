8e629098adf02c4315612423253bd8dc696884a3
Per Jensen <per@zapro.dk>
2025-02-20 11:38:50 +0100

    Update screenSegmented.c
    Added some macros to make the byte structure more readable. trying to understand the LUTs better.

--- custom-firmware/UART_SEGMENT_EPD/screenSegmented.c
+++ custom-firmware/UART_SEGMENT_EPD/screenSegmented.c
@@ -7,6 +7,17 @@
 __bit display_is_drawing = 0;
 uint32_t screen_start_time = 0;
 
+extern bool lut5_active;
+
+#define B24(a, b, c) (((uint32_t)(a) << 16) | ((uint32_t)(b) << 8) | (c))
+#define B24_6BIT(a, b, c, d) (((uint32_t)(a) << 18) | ((uint32_t)(b) << 12) | ((uint32_t)(c) << 6) | (d))
+#define B24_10_10_4(a, b, c) (((uint32_t)(a) << 14) | ((uint32_t)(b) << 4) | (c))
+#define B24_8_2_8_2_4(a, b, c, d, e) (((uint32_t)(a) << 16) | \
+                                      ((uint32_t)(b & 0x3) << 14) | \
+                                      ((uint32_t)(c) << 6)  | \
+                                      ((uint32_t)(d & 0x3) << 4)  | \
+                                      ((uint32_t)(e & 0xF)))
+
 #pragma callee_saves screenPrvTimedWait
 static __bit screenPrvTimedWait(uint32_t maxTicks)
 {
@@ -68,14 +79,14 @@ __bit screenDraw(const uint8_t __xdata *data, __bit inverted, __bit custom_lut)
 	if (!screenPrvRegWrite(0x04, 0xfc0000))
 		return false;
 
-	// data (doesnt agree with spec but makes sense)
-	if (!screenPrvRegWrite(0x0d, *(uint32_t __xdata *)(data + 0)))
+	// data
+	if (!screenPrvRegWrite(0x0d, *(uint32_t __xdata *)(data + 0))) // Data register 0
 		return false;
-	if (!screenPrvRegWrite(0x0e, *(uint32_t __xdata *)(data + 3)))
+	if (!screenPrvRegWrite(0x0e, *(uint32_t __xdata *)(data + 3))) // Data register 1
 		return false;
-	if (!screenPrvRegWrite(0x0f, *(uint32_t __xdata *)(data + 6)))
+	if (!screenPrvRegWrite(0x0f, *(uint32_t __xdata *)(data + 6))) // Data register 2
 		return false;
-	if (!screenPrvRegWrite(0x10, *(uint32_t __xdata *)(data + 9)))
+	if (!screenPrvRegWrite(0x10, *(uint32_t __xdata *)(data + 9))) // Data register 3
 		return false;
 	if (custom_lut)
 	{
@@ -92,28 +103,73 @@ __bit screenDraw(const uint8_t __xdata *data, __bit inverted, __bit custom_lut)
 		if (!screenPrvRegWrite(0x1a, *(uint32_t __xdata *)(data + 24)))
 			return false;
 
+#define UPDATE_NORMAL B24(0b00100000, 0x0, 0x0)
+#define UPDATE_INVERT B24(0x0, 0x0, 0x0)
+
 		// update!
-		if (!screenPrvRegWrite(0x00, *(uint32_t __xdata *)(data + 27) | (inverted ? 0x200000 : 0x000000)))
+		if (!screenPrvRegWrite(0x00, *(uint32_t __xdata *)(data + 27) | (inverted ? UPDATE_NORMAL : UPDATE_INVERT)))
 			return false;
 	}
 	else
-	{
+#define LUT1_NORMAL B24_8_2_8_2_4(0b01000100, 0x00, 0x0, 0x00, 0x0000)
+
+#define LUT2_NORMAL B24_8_2_8_2_4(0b01101000, 0x00, 0x0, 0x00, 0x0001)
+#define LUT2_INVERT B24_8_2_8_2_4(0b10000110, 0x00, 0x0, 0x00, 0x0000)
+
+#define LUT3_NORMAL B24_8_2_8_2_4(0b00100100, 0x00, 0x0, 0x00, 0x0000)
+#define LUT3_INVERT B24_8_2_8_2_4(0b00000100, 0x00, 0x0, 0x00, 0x0000)
 		// LUTs
-		if (!screenPrvRegWrite(0x14, 0x440000))
+		if (!screenPrvRegWrite(0x14, LUT1_NORMAL)) // RPT 0xF
 			return false;
-		if (!screenPrvRegWrite(0x15, inverted ? 0x680001 : 0x860000))
+		if (!screenPrvRegWrite(0x15, inverted ? LUT2_NORMAL : LUT2_INVERT))
 			return false;
-		if (!screenPrvRegWrite(0x16, inverted ? 0x240000 : 0x040000))
+		if (!screenPrvRegWrite(0x16, inverted ? LUT3_NORMAL : LUT3_INVERT))
 			return false;
 
-		if (!screenPrvRegWrite(0x19, 0x082514))
+#define LUT5_NORMAL B24_8_2_8_2_4(0b10000110, 0x01, 0x0, 0x00, 0x0000)
+#define LUT5_INVERT B24_8_2_8_2_4(0b01101000, 0x01, 0x0, 0x00, 0x0000)
+
+		//-----------------------------------------------------------------------------------------------------------------------
+		if (!screenPrvRegWrite(0x18, inverted ? LUT5_NORMAL : LUT5_INVERT)) // Waveform LUT5
 			return false;
-		if (!screenPrvRegWrite(0x1a, 0xa02000))
+#define LUT8 B24_6BIT(0b010100, 0x0, 0x0, 0x0)
+
+		if (!screenPrvRegWrite(0x1b, LUT8)) // Waveform LUT8 - Timing 3
 			return false;
+		//-----------------------------------------------------------------------------------------------------------------------
+#define LUT6 B24_6BIT(0b000010, 0b000010, 0b010100, 0b010100)
+#define LUT7 B24_6BIT(0b101000, 0b000010, 0x0, 0x0)
+		if (!screenPrvRegWrite(0x19, LUT6)) // Waveform LUT6 - Timing 1
+			return false;
+		if (!screenPrvRegWrite(0x1a, 0xa02000)) // Waveform LUT7 - Timing 2
+			return false;
+
+
 
 		// update!
-		if (!screenPrvRegWrite(0x00, inverted ? 0xa0001c : 0x80001c))
-			return false;
+		if (lut5_active)
+		{
+#undef 	UPDATE_NORMAL
+#undef 	UPDATE_INVERT
+#define UPDATE_NORMAL B24(0b10100000, 0x0, 0x1)
+#define UPDATE_INVERT B24(0b10000000, 0x0, 0x1)
+			//-----------------------------------------------------------------------------------------------------------------------
+			if (!screenPrvRegWrite(0x00, inverted ? UPDATE_NORMAL : UPDATE_INVERT)) // Only use LUT5.
+				return false;
+			//-----------------------------------------------------------------------------------------------------------------------
+		}
+		else
+		{
+#undef 	UPDATE_NORMAL
+#undef 	UPDATE_INVERT
+//#define UPDATE_NORMAL B24(0b10100000, 0x0, 0b00011100)
+//#define UPDATE_INVERT B24(0b10000000, 0x0, 0b00011100)
+#define UPDATE_NORMAL B24(0b10100000, 0x0, 0b00011100)
+#define UPDATE_INVERT B24(0b10000000, 0x0, 0b00011100)
+
+			if (!screenPrvRegWrite(0x00, inverted ? UPDATE_NORMAL : UPDATE_INVERT)) // LUT 1,2,3 used.
+				return false;
+		}
 	}
 	timerDelay(TIMER_TICKS_PER_SECOND / 1000);
 
